56c60bd2f0ca07753d0ab8bcbeb061c4aa54e6fb
Patrick Cruz <patrickcruz@MacBook-Pro-de-Patrick.local>
2020-08-07 00:08:12 -0300

    Aula 5

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import CadastroVideo from './pages/Cadastro/Video'
-import CadastroCategoria from './pages/Cadastro/Categoria'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  BrowserRouter
-} from "react-router-dom";
+import CadastroVideo from './pages/Cadastro/Video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
 
-
-//desafio criar game para página de erro
+// Desafio master blaster na descrição
 const Pagina404 = () => (<div>Página 404</div>)
-
 
 ReactDOM.render(
   <BrowserRouter>
-  <Switch>
-  <Route path="/" component={Home} exact/>
-  <Route path="/cadastro/video" component={CadastroVideo} />
-  <Route path="/cadastro/categoria" component={CadastroCategoria} />
-  <Route component={Pagina404} />
-  </Switch>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-
